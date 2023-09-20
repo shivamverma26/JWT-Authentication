197d9be5ab751e3df8ea8bc659ec5f22d447e0c1
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-09-21 01:21:33 +0530

    JWT Authentication with Spring Boot

---
+++ src/main/java/com/jwt_authenticate/jwt/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.jwt_authenticate.jwt.dao;
+
+import com.jwt_authenticate.jwt.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<User, String> {
+}
---
+++ src/main/java/com/jwt_authenticate/jwt/service/RoleService.java
@@ -0,0 +1,17 @@
+package com.jwt_authenticate.jwt.service;
+
+import com.jwt_authenticate.jwt.dao.RoleDao;
+import com.jwt_authenticate.jwt.entity.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public Role createNewRole(Role role) {
+        return roleDao.save(role);
+    }
+}
