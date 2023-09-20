3aea9dbcf6c0c3346dfbef55b918ebd83f87fda0
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-09-21 01:37:00 +0530

    Delete src directory

--- src/main/java/com/jwt_authenticate/jwt/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.jwt_authenticate.jwt.dao;
-
-import com.jwt_authenticate.jwt.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
