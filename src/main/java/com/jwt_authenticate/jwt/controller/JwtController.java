5889b2a6fafb43ee3e4fd3dad30185c9ae8ce585
shivamverma26 <94590743+shivamverma26@users.noreply.github.com>
2023-09-21 01:46:40 +0530

    JWT Authentication with Spring Boot

---
+++ src/main/java/com/jwt_authenticate/jwt/controller/JwtController.java
@@ -0,0 +1,23 @@
+package com.jwt_authenticate.jwt.controller;
+
+import com.jwt_authenticate.jwt.entity.JwtRequest;
+import com.jwt_authenticate.jwt.entity.JwtResponse;
+import com.jwt_authenticate.jwt.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class JwtController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostMapping({"/authenticate"})
+    public JwtResponse createJwtToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+        return jwtService.createJwtToken(jwtRequest);
+    }
+}
---
+++ src/main/java/com/jwt_authenticate/jwt/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package com.jwt_authenticate.jwt.dao;
+
+import com.jwt_authenticate.jwt.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleDao extends CrudRepository<Role, String> {
+
+}
